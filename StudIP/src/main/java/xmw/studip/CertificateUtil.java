a83dfd9436525a598c16661ee243c6a2b874afa7
frederik.hennecke <frederik.hennecke@stud.uni-goettingen.de>
2025-01-18 12:11:14 +0100

    added multiple templates for certificates

--- StudIP/src/main/java/xmw/studip/CertificateUtil.java
+++ StudIP/src/main/java/xmw/studip/CertificateUtil.java
@@ -12,15 +12,15 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @WebServlet("/generateCertificate")
 public class CertificateUtil extends HttpServlet {
+
+    private static final String TEMPLATE_DIR = "/templates/";
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -33,18 +33,18 @@ public class CertificateUtil extends HttpServlet {
             List<Map<String, String>> grades = xmlDatabase.getGrades(userId);
             List<Map<String, String>> exams = xmlDatabase.getExams();
 
-            for (Map<String, String> map1 : grades) {
-                for (Map<String, String> map2 : exams) {
-                    if (map1.containsKey("id") && map2.containsKey("examId") &&
-                            map1.get("id").equals(map2.get("examId"))) {
-
-                        map1.putAll(map2);
+            // Merge exam details into grades
+            for (Map<String, String> grade : grades) {
+                for (Map<String, String> exam : exams) {
+                    if (grade.get("id").equals(exam.get("examId"))) {
+                        grade.putAll(exam);
                     }
                 }
             }
 
-            // Generate certificate
-            String certificateHtml = generateCertificate(userInfo, grades);
+            // Determine the user's field of study and choose the template
+            String fieldOfStudy = userInfo.getOrDefault("faculty", "default").toLowerCase();
+            String certificateHtml = generateCertificate(userInfo, grades, fieldOfStudy);
 
             // Write certificate HTML to response
             response.setContentType("text/html");
@@ -57,121 +57,54 @@ public class CertificateUtil extends HttpServlet {
         }
     }
 
-    public static String generateCertificate(Map<String, String> userInfo, List<Map<String, String>> grades) {
-        try {
-            // Build XML data
-            StringBuilder xmlData = new StringBuilder();
-            xmlData.append("<Certificate>");
-            xmlData.append("<User>");
-            for (Map.Entry<String, String> entry : userInfo.entrySet()) {
+    private String generateCertificate(Map<String, String> userInfo, List<Map<String, String>> grades, String fieldOfStudy) throws Exception {
+        // Build XML data
+        StringBuilder xmlData = new StringBuilder();
+        xmlData.append("<Certificate>");
+        xmlData.append("<User>");
+        for (Map.Entry<String, String> entry : userInfo.entrySet()) {
+            xmlData.append(String.format("<%s>%s</%s>", entry.getKey(), entry.getValue(), entry.getKey()));
+        }
+        xmlData.append("</User>");
+        xmlData.append("<Grades>");
+        for (Map<String, String> grade : grades) {
+            xmlData.append("<Grade>");
+            for (Map.Entry<String, String> entry : grade.entrySet()) {
                 xmlData.append(String.format("<%s>%s</%s>", entry.getKey(), entry.getValue(), entry.getKey()));
             }
-            xmlData.append("</User>");
-            xmlData.append("<Grades>");
-            for (Map<String, String> grade : grades) {
-                xmlData.append("<Grade>");
-                for (Map.Entry<String, String> entry : grade.entrySet()) {
-                    xmlData.append(String.format("<%s>%s</%s>", entry.getKey(), entry.getValue(), entry.getKey()));
-                }
-                xmlData.append("</Grade>");
-            }
-            xmlData.append("</Grades>");
-            xmlData.append("</Certificate>");
-
-            // XSLT to transform XML into HTML
-            String xslt = """
-                <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-                    <xsl:output method="html" encoding="UTF-8" indent="yes"/>
-                    <xsl:template match="/Certificate">
-                        <html>
-                            <head>
-                                <title>Certificate of Grades</title>
-                                <link rel="stylesheet" href="https://latex.vercel.app/style.css"/>
-                                <style>
-                                    body {
-                                        text-align: center;
-                                        background-color: #3366CC;
-                                    }
-                                    .certificate-container {
-                                        max-width: 800px;
-                                        margin: auto;
-                                        background: #ffffff;
-                                        padding: 20px;
-                                        border-radius: 10px;
-                                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                                    }
-                                    h1, h2 {
-                                        color: #333;
-                                    }
-                                    table {
-                                        margin: auto;
-                                        width: 90%;
-                                        border-collapse: collapse;
-                                    }
-                                    th, td {
-                                        border: 1px solid #ddd;
-                                        padding: 10px;
-                                        text-align: left;
-                                    }
-                                    th {
-                                        background-color: #f2f2f2;
-                                    }
-                                </style>
-                            </head>
-                            <body>
-                                <div class="certificate-container">
-                                    <h1>Certificate of Academic Performance</h1>
-                                    <p>This document certifies the academic achievements of the student listed below:</p>
-                                    <h2>Student Information</h2>
-                                    <table>
-                                        <tr><th>Field</th><th>Value</th></tr>
-                                        <xsl:for-each select="User/*">
-                                            <tr>
-                                                <td><xsl:value-of select="name()"/></td>
-                                                <td><xsl:value-of select="."/></td>
-                                            </tr>
-                                        </xsl:for-each>
-                                    </table>
-                                    <h2>Grades</h2>
-                                    <p>The following table lists the courses completed along with the grades achieved:</p>
-                                    <table>
-                                        <tr>
-                                            <th>Course</th>
-                                            <th>Grade</th>
-                                            <th>Exam ID</th>
-                                        </tr>
-                                        <xsl:for-each select="Grades/Grade">
-                                            <tr>
-                                                <td><xsl:value-of select="courseName"/></td>
-                                                <td><xsl:value-of select="grade"/></td>
-                                                <td><xsl:value-of select="id"/></td>
-                                            </tr>
-                                        </xsl:for-each>
-                                    </table>
-                                    <p>Issued on: 2025-01-01</p>
-                                    <p>Certified by: <strong>University Administration</strong></p>
-                                </div>
-                            </body>
-                        </html>
-                    </xsl:template>
-                </xsl:stylesheet>
-                """;
-
-            // Transform XML using XSLT
-            TransformerFactory factory = TransformerFactory.newInstance();
-            Transformer transformer = factory.newTransformer(new StreamSource(new java.io.StringReader(xslt)));
-
-            Source xmlSource = new StreamSource(new java.io.StringReader(xmlData.toString()));
-            StringWriter output = new StringWriter();
-            Result result = new StreamResult(output);
-
-            transformer.transform(xmlSource, result);
-
-            return output.toString(); // Return HTML as a String
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "<html><body><h1>Error generating certificate</h1></body></html>";
+            xmlData.append("</Grade>");
         }
+        xmlData.append("</Grades>");
+        xmlData.append("</Certificate>");
+
+        // Load the appropriate XSLT template
+        String templatePath = getTemplatePath(fieldOfStudy);
+        Transformer transformer = TransformerFactory.newInstance().newTransformer(new StreamSource(new File(templatePath)));
+
+        // Transform XML using XSLT
+        Source xmlSource = new StreamSource(new StringReader(xmlData.toString()));
+        StringWriter output = new StringWriter();
+        Result result = new StreamResult(output);
+
+        transformer.transform(xmlSource, result);
+
+        return output.toString(); // Return HTML as a String
     }
 
+    private String getTemplatePath(String fieldOfStudy) {
+        // Map the field of study to the corresponding template
+        String fileName;
+        switch (fieldOfStudy) {
+            case "computer science":
+                fileName = "computer_science.xslt";
+                break;
+            case "mathematics":
+                fileName = "mathematics.xslt";
+                break;
+            default:
+                fileName = "default.xslt";
+                break;
+        }
+        return getServletContext().getRealPath(TEMPLATE_DIR + fileName);
+    }
 }
