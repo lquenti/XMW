a585ee6a2e13a11767261c79f26eca96b6fe0ed1
Valerius Mattfeld <mail@valerius.me>
2025-01-19 12:11:57 +0100

    POST /semesters

--- EXA/src/main/java/xmw/exa/models/semesters/SemestersServlet.java
+++ EXA/src/main/java/xmw/exa/models/semesters/SemestersServlet.java
@@ -6,10 +6,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import xmw.exa.db.DB;
 import xmw.exa.util.ExaServlet;
 import xmw.exa.util.Util;
-import xmw.flush.Semesters;
+import xmw.flush.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(name = "semesters", value = "/semesters")
 public class SemestersServlet extends ExaServlet {
@@ -31,7 +33,76 @@ public class SemestersServlet extends ExaServlet {
         out.flush();
     }
 
-      @Override
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final String[] requiredParams = {"name", "start", "end"};
+        final Map<String, String> defaultRawDto = new HashMap<>();
+        defaultRawDto.put("name", "");
+        defaultRawDto.put("start", "");
+        defaultRawDto.put("end", "");
+
+        var rawDto = Util.getRawDto(defaultRawDto, requiredParams, request, response);
+        if (rawDto == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        // Verify that start and end are valid dates
+        if (!Util.verifyDate(rawDto.get("start")) || !Util.verifyDate(rawDto.get("end"))) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        // Verify that the semester does not already exist
+        boolean exists = false;
+
+        for (var semester : db.semesters().all()) {
+            if (semester.getNameOrStartOrEnd().getFirst().equals(rawDto.get("name"))) {
+                exists = true;
+                break;
+            }
+            if (semester.getNameOrStartOrEnd().get(1).equals(rawDto.get("name"))) {
+                exists = true;
+                break;
+            }
+            if (semester.getNameOrStartOrEnd().get(2).equals(rawDto.get("name"))) {
+                exists = true;
+                break;
+            }
+        }
+
+        if (exists) {
+            response.setStatus(HttpServletResponse.SC_CONFLICT);
+            return;
+        }
+
+        // Create the semester
+        Semester semester = new Semester();
+        Name name = new Name();
+        name.setContent(rawDto.get("name"));
+        semester.getNameOrStartOrEnd().add(name);
+        Start start = new Start();
+        start.setContent(rawDto.get("start"));
+        semester.getNameOrStartOrEnd().add(start);
+        End end = new End();
+        end.setContent(rawDto.get("end"));
+        semester.getNameOrStartOrEnd().add(end);
+
+        // Add the semester
+        boolean success = db.semesters().create(semester);
+        if (!success) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+
+        response.setStatus(HttpServletResponse.SC_CREATED);
+        response.setContentType("application/xml");
+        PrintWriter out = response.getWriter();
+        out.println(DB.marshal(semester));
+        out.flush();
+    }
+
+    @Override
     public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Util.deleteItem(db.semesters(), request, response);
     }
